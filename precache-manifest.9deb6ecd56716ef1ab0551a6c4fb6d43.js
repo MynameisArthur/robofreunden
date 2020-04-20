c7b6d018b6352c746583280d66efc6d8cbbcba05
Arthur <arthurd800@wp.pl>
2020-04-20 09:52:32 +0200

    Updates

---
+++ precache-manifest.9deb6ecd56716ef1ab0551a6c4fb6d43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d762017194837ba8571ed80a5990ea3",
+    "url": "/robofreunden/index.html"
+  },
+  {
+    "revision": "a6ac29b4190b2e492276",
+    "url": "/robofreunden/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "2313868f6190f952b4b2",
+    "url": "/robofreunden/static/css/main.e63237e9.chunk.css"
+  },
+  {
+    "revision": "a6ac29b4190b2e492276",
+    "url": "/robofreunden/static/js/2.c3bd24be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofreunden/static/js/2.c3bd24be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2313868f6190f952b4b2",
+    "url": "/robofreunden/static/js/main.0938b361.chunk.js"
+  },
+  {
+    "revision": "b3f6197bed2667707302",
+    "url": "/robofreunden/static/js/runtime-main.d83553cf.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofreunden/static/media/SEGA.12a3db8b.woff"
+  }
+]);
