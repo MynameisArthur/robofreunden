3771ad7e3980563c778172360eeeb3e6c78ed438
Arthur <arthurd800@wp.pl>
2020-05-23 10:39:24 +0200

    optimized app: updated Card and Scroll components, updated meta tag=description, added font-display rule in @font-face

--- src/components/SearchBox.component.jsx
+++ src/components/SearchBox.component.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-const SearchBox = ({searchfield,searchChange})=> {
-    console.log('SearchBox');    
+const SearchBox = ({searchfield,searchChange})=> {      
     return (
         <div className="pa2">
-            <input type="search" placeholder="Search robot" className="pa3 ba b--green bg-lightest-blue" value={searchfield} onChange={searchChange}/>
+            <input 
+                aria-label="Search Robots"
+                type="search" 
+                placeholder="Search robot" 
+                className="pa3 ba b--green bg-lightest-blue" 
+                value={searchfield} 
+                onChange={searchChange}
+            />
         </div>
     );
 };
